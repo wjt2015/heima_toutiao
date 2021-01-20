f08870b1e5e7e2f6cc2c849d961bf79946d084ec
王锦涛 <jintao9@staff.weibo.com>
2021-01-21 00:39:00 +0800

    添加corsfilter

--- src/main/java/toutiao/config/MyAnnotationConfigDispatcherServletInitializer.java
+++ src/main/java/toutiao/config/MyAnnotationConfigDispatcherServletInitializer.java
@@ -1,7 +1,20 @@
 package toutiao.config;
 
+import com.google.common.collect.Lists;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.CorsUtils;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+import toutiao.filter.MyCorsFilter;
+
+import javax.servlet.DispatcherType;
+import javax.servlet.Filter;
+import javax.servlet.FilterRegistration;
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import java.util.EnumSet;
 
 
 @Slf4j
@@ -20,4 +33,38 @@ public class MyAnnotationConfigDispatcherServletInitializer extends AbstractAnno
     protected String[] getServletMappings() {
         return new String[]{"*.json"};
     }
+
+    @Override
+    protected Filter[] getServletFilters() {
+        /**
+         * 添加corsFilter;
+         */
+/*        CorsConfigurationSource corsConfigurationSource = new CorsConfigurationSource() {
+            @Override
+            public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+                CorsConfiguration corsConfiguration = new CorsConfiguration();
+                if (CorsUtils.isCorsRequest(request)) {
+                    corsConfiguration.setAllowedOrigins(Lists.newArrayList(CorsConfiguration.ALL));
+                    return corsConfiguration;
+                }
+                return null;
+            }
+        };
+
+        MyCorsFilter corsFilter = new MyCorsFilter();
+        return new Filter[]{corsFilter};
+        */
+
+        Filter[] filters = new Filter[]{new MyCorsFilter()};
+        log.info("filters={};", filters);
+        return filters;
+    }
+
+    @Override
+    protected FilterRegistration.Dynamic registerServletFilter(ServletContext servletContext, Filter filter) {
+        FilterRegistration.Dynamic dynamic = super.registerServletFilter(servletContext, filter);
+        dynamic.addMappingForUrlPatterns(EnumSet.allOf(DispatcherType.class), true, "/*");
+        return dynamic;
+    }
+
 }
---
+++ src/main/java/toutiao/filter/MyCorsFilter.java
@@ -0,0 +1,68 @@
+package toutiao.filter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Service;
+import org.springframework.web.cors.CorsUtils;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * 跨域详解 been blocked by CORS policy: No 'Access-Control-Allow-Origin' header is present on the requested resource
+ * (https://www.cnblogs.com/cxygg/p/12419502.html);
+ * <p>
+ * cors,
+ * (https://juejin.cn/post/6844903873400799240);
+ * (https://juejin.cn/post/6844903568852385806);
+ * (https://juejin.cn/post/6844903838621630477);
+ * (https://juejin.cn/post/6887744164079878151);
+ * (https://juejin.cn/search?query=cors&type=all);
+ * <p>
+ * <p>
+ * //---
+ * DelegatingFilterProxy;
+ */
+@Slf4j
+@Order(value = 1)
+@Service
+//@WebFilter(urlPatterns = {"/*"})
+public class MyCorsFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        log.info("corsFilter init finish!");
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+
+        final long start = System.currentTimeMillis();
+
+        HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+        HttpServletResponse httpServletResponse = (HttpServletResponse) response;
+
+        log.info("doFilter!requestUrl={};", httpServletRequest.getRequestURL());
+        if (CorsUtils.isCorsRequest(httpServletRequest)) {
+            httpServletResponse.setHeader("Access-Control-Allow-Origin", "*"); //  这里最好明确的写允许的域名
+            httpServletResponse.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT");
+            httpServletResponse.setHeader("Access-Control-Max-Age", "3600");
+            httpServletResponse.setHeader("Access-Control-Allow-Headers", "Content-Type,Access-Token,Authorization");
+        }
+
+        chain.doFilter(request, response);
+        log.info("cors allow!requestURL={};method={};protocol={};elapsed={}ms;", httpServletRequest.getRequestURL(), httpServletRequest.getMethod(), httpServletRequest.getProtocol(), (System.currentTimeMillis() - start));
+
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
